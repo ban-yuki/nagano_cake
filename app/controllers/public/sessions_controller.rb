aed49ec155a233af48f74a32577285a7d210e36a
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-10-19 11:22:14 +0000

    [Add] devise

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,13 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # GET /resource/sign_in
   # def new
