58fe12cffdbe290efcb0ff9570433e78857c1722
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-10-22 06:14:59 +0000

    [Add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  namespace :public do
+    patch 'customers/update'
+    patch 'customers/withdraw'
+    resources :customers, only: [:show, :edit, :confirm_withdraw]
+  end
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
