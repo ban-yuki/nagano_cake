eebf69736144c1408d2a4d71a7b746136fcc168b
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-10-22 11:34:52 +0000

    [Add] 4-1

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,8 @@
 class Public::CustomersController < ApplicationController
+  before_action :is_matching_login_user
+
   def show
+    @public = Public.find(params[:id])
   end
 
   def edit
@@ -13,4 +16,7 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
+  
+  protected
+   
 end
