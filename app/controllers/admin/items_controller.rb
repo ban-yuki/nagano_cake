08e948f3fb35a7a0b1cd077ca5ab006cea664db7
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-10-23 11:15:36 +0000

    [Add] 4-1

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  
+  has_one_attached :image
+  
+  def new
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,8 @@
 class Public::CustomersController < ApplicationController
-  before_action :is_matching_login_user
+  #before_action :is_matching_login_user
 
   def show
-    @public = Public.find(params[:id])
+    #@public = Public.find(params[:id])
   end
 
   def edit
@@ -16,7 +16,7 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
-  
+
   protected
-   
+
 end
