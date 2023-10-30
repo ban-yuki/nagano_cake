55d6eaa3af4c67fa5c80fedda0c517fba4f69e7f
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-10-30 11:57:20 +0000

    [Add] 6

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,32 +1,32 @@
 class Admin::ItemsController < ApplicationController
-  
+
   #has_one_attached :image
-  
+
   def new
-    @item = Item.new 
+    @item = Item.new
   end
-  
+
   def index
     @items = Item.page(params[:page])
   end
-  
+
   def show
-    @item = Item.find(params[:id])  
+    @item = Item.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(item_params[:item])
     @item.save
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:name, :introduction, :price)
   end
