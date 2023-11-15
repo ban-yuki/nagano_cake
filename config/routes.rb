f3a9127928951967647b505b4fff47b63e1e41ef
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-15 11:12:53 +0000

    [Add] 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
-
+  
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
   namespace :public do
     patch 'current_customers/update'
-    get '/customers/sign_up', to: 'public/registrations#new'
-    post '/customers', to: 'public/registrations#create'
-    get 'sessions/new'
+    patch 'current_customers' => 'public/customers#update'
     get  '/current_customers/confirm_withdraw' => 'customers#confirm_withdraw'
     patch  '/current_customers/withdraw' => 'customers#withdraw'
     resources :customers, only: [:show, :edit]
     resources :items, only: [:index, :show,]
   end
-  devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
   }
-
   namespace :admin do
     post 'items/create' => 'items#create'
     patch 'items/:id' => 'items#update'
@@ -22,10 +24,6 @@ Rails.application.routes.draw do
     resources :items, only: [:new, :index, :show, :edit]
     resources :customers, only: [:index, :show, :edit]
   end
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-  }
-
-
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
