def7bb5a160f358c120dd3218d47fe70e88c8a3e
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-16 06:55:51 +0000

    [Add] 6

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -36,7 +36,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :password, :password_confirmation)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation)
   end
 
   # GET /resource/sign_up
