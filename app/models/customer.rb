00dde600703cf5ac28afbbf9fda28e75fcfc7653
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-16 08:24:53 +0000

    [Add] 6

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   def active_for_authentication?
     super && (is_active == true)
-  end       
+  end
+  
+  def full_name
+    "#{last_name} #{first_name}"
+  end
+  
 end
+
