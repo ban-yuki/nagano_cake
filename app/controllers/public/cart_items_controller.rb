41b12eb9ecdc9d1ce432059b8c57b9c7f5f276c2
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-17 12:24:50 +0000

    [Add] 7

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,36 @@
 class Public::CartItemsController < ApplicationController
+  
+  def index
+    @cart_items = Cart_item.all
+  end
+
+  def create
+    @cart_item = Cart_item.new(cart_item_params)
+    if @cart_item.save 
+    redirect_to admin_item_path(@cart_item)
+    else
+    render :new
+    end
+  end
+
+  def update
+    @cart_item = Cart_item.find(params[:id])
+    if @cart_item.update(cart_item_params)
+    redirect_to admin_items_path(@cart_item)
+    else
+    @cart_item = Cart_item.all
+    render :edit
+    end
+  end
+  
+  def destroy
+    @cart_item = Cart_item.find(params[:id])
+  end
+  
+  def destroy_all
+    @cart_items = Cart_items
+  end
+
+  private
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,20 @@ Rails.application.routes.draw do
   }
 
   scope module: :public do
-    patch 'current_customers/update'
-    patch 'current_customers' => 'public/customers#update'
+    patch '/current_customers/update'
+    patch '/current_customers' => 'public/customers#update'
     get  '/current_customers/confirm_withdraw' => 'customers#confirm_withdraw'
     patch  '/current_customers/withdraw' => 'customers#withdraw'
     post '/orders/confirm' => 'orders#confirm'
     post '/orders' => 'orders#create'
+    patch '/cart_items/:id/update' => 'cart_items#update'
+    post '/cart_items' => 'cart_items#create'
+    delete '/cart_items/:id/update' => 'cart_items#destroy'
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :customers, only: [:show, :edit]
     resources :items, only: [:index, :show]
     resources :orders, only: [:new, :index, :show, :confirm_orders]
+    resources :cart_items, only: [:index]
   end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
