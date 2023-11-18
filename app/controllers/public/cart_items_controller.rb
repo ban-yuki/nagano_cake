da73986bfffbf3dd6c0dbaa06f73c5a15f418dd9
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-18 12:41:11 +0000

    [Add] 7

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,7 +2,7 @@ class Public::CartItemsController < ApplicationController
   
   def index
     @cart_items = CartItem.all
-    @item = Item.find(params[:id])
+    @item = Item.all
     @total = 0
   end
 
