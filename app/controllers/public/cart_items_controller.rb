33773c06e7f632968a57586b32167d4be6164fdf
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-18 11:58:56 +0000

    [Add] 7

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,34 +1,48 @@
 class Public::CartItemsController < ApplicationController
   
   def index
-    @cart_items = Cart_item.all
+    @cart_items = CartItem.all
+    @item = Item.find(params[:id])
+    @total = 0
   end
 
   def create
-    @cart_item = Cart_item.new(cart_item_params)
-    if @cart_item.save 
-    redirect_to admin_item_path(@cart_item)
-    else
-    render :new
-    end
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_items=current_customer.cart_items.all
+      @cart_items.each do |cart_item|
+        if cart_item.item_id==@cart_item.item_id
+        new_quantity = cart_item.quantity + @cart_item.quantity
+        cart_item.update_attribute(:quantity, new_quantity)
+        @cart_item.delete
+        end
+      end
+      @cart_item.save
+      redirect_to cart_items_path,notice:"カートに商品が入りました"
   end
 
   def update
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     if @cart_item.update(cart_item_params)
     redirect_to admin_items_path(@cart_item)
     else
-    @cart_item = Cart_item.all
+    @cart_item = CartItem.all
     render :edit
     end
   end
   
   def destroy
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    @cart_items = CartItem.all
+  　render 'index'
   end
   
   def destroy_all
-    @cart_items = Cart_items
+    @cart_items = CartItems
+    cart_items = CartItem.all
+    cart_items.destroy_all
+  　render 'index'
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,20 @@ class Item < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
 
-   def get_image
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
+  def subtotal
+    item.with_tax_price * amount
   end
 
 end
