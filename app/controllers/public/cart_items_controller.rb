bc4d5c313705f12624293113739bc7870766ad98
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-11-29 11:32:32 +0000

    [Add] 9

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -23,30 +23,30 @@ class Public::CartItemsController < ApplicationController
           @cart_item.delete
         end
       end
-    end
   end
-  
+
   def create
-	if current_member.cart_items.count >= 1 #カート内に商品があるか？
-	  if nil != current_member.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カートに入れた商品はすでにカートに追加済か？
-		   @cart_item_u = current_member.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カート内のすでにある商品の情報取得
+	  current_customer.cart_items.count >= 1 #カート内に商品があるか？
+	  if nil != current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カートに入れた商品はすでにカートに追加済か？
+		   @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カート内のすでにある商品の情報取得
 		   @cart_item_u.number_of_items += params[:cart_item][:number_of_items].to_i #既にある情報に個数を合算
 		   @cart_item_u.update(number_of_items: @cart_item_u.number_of_items) #情報の更新　個数カラムのみ
 		   redirect_to public_cart_items_path #カートページ遷移
-		else
-			@cart_item = CartItem.new(cart_item_params) #新しくカートの作成
-			@cart_item.member_id = current_member.id #誰のカートか紐付け
-			if @cart_item.save #情報を保存できるか？
-				 redirect_to public_cart_items_path #カートページ遷移
-			else
-				@items = Item.where(sale_status: 0).page(params[:page]).per(8) #販売ステータスが「0」のものを見つける
-		    @quantity = Item.count #商品の数をカウント
-				render 'index' #indexアクションを呼び出す
-		  end
+		  else
+			  @cart_item = CartItem.new(cart_item_params) #新しくカートの作成
+			  @cart_item.customer_id = current_customer.id #誰のカートか紐付け
+			    if @cart_item.save #情報を保存できるか？
+    			  flash[:notice] = 'カートに商品が入りました。'
+    				 redirect_to public_cart_items_path #カートページ遷移
+    			else
+    		    @quantity = Item.count #商品の数をカウント
+    				render 'index' #indexアクションを呼び出す
+		      end
 	  end
+	end
 
 
-	
+
   def update
     @cart_item = CartItem.find(params[:id])
     if @cart_item.update(cart_item_params)
@@ -75,4 +75,5 @@ class Public::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount, :quantity, :customer_id)
   end
-
+  
+end
