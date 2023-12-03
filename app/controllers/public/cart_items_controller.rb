ff01fa99fd1cf70e89a6c64ff27ad75917c8d7aa
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-12-03 12:40:57 +0000

    [Add] 9

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,45 +7,22 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
     @cart_item.customer_id=current_customer.id
-      @cart_items.each do |cart_item|
-        if  @cart_item.save
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+        cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+        cart_item.quantity += params[:cart_item][:quantity].to_i
+        cart_item.save
           flash[:notice] = 'カートに商品が入りました。'
           redirect_to cart_items_path
-        else
-          flash[:alert] = '商品の追加に失敗しました。'
-          render :show
-        end
-        unless @cart_items.pluck(:item_id).include?(@cart_item.item_id)
-          new_quantity = cart_item.quantity + @cart_item.quantity
-          cart_item.update_attribute(:quantity, new_quantity)
-          @cart_item.delete
-        end
-      end
-  end
-
-  def create
-	  current_customer.cart_items.count >= 1 #カート内に商品があるか？
-	  if nil != current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カートに入れた商品はすでにカートに追加済か？
-		   @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カート内のすでにある商品の情報取得
-		   @cart_item_u.number_of_items += params[:cart_item][:number_of_items].to_i #既にある情報に個数を合算
-		   @cart_item_u.update(number_of_items: @cart_item_u.number_of_items) #情報の更新　個数カラムのみ
-		   redirect_to public_cart_items_path #カートページ遷移
-		else
-			@cart_item = CartItem.new(cart_item_params) #新しくカートの作成
-			  @cart_item.customer_id = current_customer.id #誰のカートか紐付け
-			    if @cart_item.save #情報を保存できるか？
-    			  flash[:notice] = 'カートに商品が入りました。'
-    				 redirect_to public_cart_items_path #カートページ遷移
-    			else
-    		    @quantity = Item.count #商品の数をカウント
-    				render 'index' #indexアクションを呼び出す
-		      end
+    elsif cart_item.save
+      @cart_items = current_customer.cart_items.all
+        redirect_to cart_items_path
+    else
+      flash[:alert] = '商品の追加に失敗しました。'
+        redirect_to item_path
     end
-	end
-
-
+  end
 
   def update
     @cart_item = CartItem.find(params[:id])
@@ -75,5 +52,5 @@ class Public::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount, :quantity, :customer_id)
   end
-  
+
 end
