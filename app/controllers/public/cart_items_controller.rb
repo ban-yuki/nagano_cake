d98a596f9b805a1f915eac4e0cd213604a8670da
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-12-04 12:22:09 +0000

    [Add] 9

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,30 +8,41 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
-    @cart_item.customer_id=current_customer.id
-    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+    if @cart_item
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id=current_customer.id
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        render :new
+      end
+    end
+    
+      @cart_item.customer_id=current_customer.id
+      if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
         cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
         cart_item.quantity += params[:cart_item][:quantity].to_i
         cart_item.save
+        flash[:notice] = 'カートに商品が入りました。'
+        redirect_to cart_items_path
+      else
+        @cart_item.quantity += params[:cart_item][:quantity].to_i
+        if @cart_item.save
           flash[:notice] = 'カートに商品が入りました。'
           redirect_to cart_items_path
-    elsif cart_item.save
-      @cart_items = current_customer.cart_items.all
-        redirect_to cart_items_path
-    else
-      flash[:alert] = '商品の追加に失敗しました。'
-        redirect_to item_path
+        else
+          flash[:alert] = '商品の追加に失敗しました。'
+          redirect_to item_path
+        end
+  
     end
   end
 
   def update
-    @cart_item = CartItem.find(params[:id])
-    if @cart_item.update(cart_item_params)
-    redirect_to admin_items_path(@cart_item)
-    else
-    @cart_items = CartItem.all
-    render :edit
-    end
+    @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+		@cart_item.update(cart_item_params)
+		redirect_to public_cart_items_path
   end
 
   def destroy
@@ -50,7 +61,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount, :quantity, :customer_id)
+    params.require(:cart_item).permit(:item_id, :quantity, :customer_id)
   end
 
 end
