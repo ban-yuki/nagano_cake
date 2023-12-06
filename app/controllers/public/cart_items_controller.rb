e85a20c5b5555198a14b0f63589c3a9da342a090
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-12-06 10:25:14 +0000

    [Add] 9

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,15 +7,12 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
-    if @cart_item.quantity += params[:cart_item][:quantity].to_i
-      current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      
-      @cart_item.customer_id=current_customer.id
-      cart_item.save
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.quantity += params[:cart_item][:quantity].to_i
+      @cart_item.save
         flash[:notice] = 'カートに商品が入りました。'
         redirect_to cart_items_path
-      
     else
       @cart_item = CartItem.new(cart_item_params)
       @cart_item.customer_id=current_customer.id
@@ -25,7 +22,7 @@ class Public::CartItemsController < ApplicationController
       else
         flash[:alert] = '商品の追加に失敗しました。'
           redirect_to item_path
-      end  
+      end
     end
   end
 
