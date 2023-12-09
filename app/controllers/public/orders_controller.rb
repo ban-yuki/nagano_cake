cec89c1a5c26e193692d38036624a04a020950d1
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-12-09 11:15:32 +0000

    [Add] 10-2

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -26,9 +26,23 @@ class Public::OrdersController < ApplicationController
   end
   
   def create
-    @order = @item.order.new(order_params)
-    @order.save
-    redirect_to items_path  
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    if @order.save
+      cart_items.each do |cart|
+        order_item = OrderItem.new
+        order_item.item_id = cart.item_id
+        order_item.order_id = @order.id
+        order_item.order_quantity = cart.quantity
+        order_item.order_price = cart.item.price
+        order_item.save
+      end
+      redirect_to orders_confirm_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
   end
   
   private
